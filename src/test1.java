3e99e1c4b17aeb42b03becd0f5806b4365fb67a0
Halit2020 <halit-altun2018@yandex.com>
2020-11-28 08:41:11 +0100

    Yeni Sepete Ekle yapildi

--- src/test1.java
+++ src/test1.java
@@ -1,6 +1,7 @@
 public class test1 {
     public static void main(String[] args) {
         System.out.println("Urunleri listele kismi yazildi");
-        System.out.println("Sepete ekle kismi yapildi");
+        System.out.println("Yeni Sepete ekle kismi yapildi");
+        System.out.println("Yeni Sepete ekle 2.kismi yapildi");
     }
 }
